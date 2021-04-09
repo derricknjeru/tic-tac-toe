43d78b80c8a192eda647e1d923e5bfde97aae400
derrick <derricknjeru5@gmail.com>
2021-04-09 16:28:29 +0300

    updated text

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{/* status  later*/}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
